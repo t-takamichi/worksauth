ac192bb75c0d993a97d6c200bcb24cc31294cb78
t-takamichi <takamichitanaka10@gmail.com>
2024-01-04 15:56:31 +1000

    delete:不要な設定を削除

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO SNSUSER (name, email, password) VALUES ('tanaka','sample01@email.com' ,'5D41402ABC4B2A76B9719D911017C592');
--- ps: hello
-INSERT INTO SNSUSER (name, email, password) VALUES ('yamada', 'sample02@email.com','5D41402ABC4B2A76B9719D911017C592');
--- ps: hello
